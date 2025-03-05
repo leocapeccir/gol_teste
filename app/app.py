3b5e9cb2cfcc96a732abf92c025b99062d14f516
leocapeccir <leocapeccir@gmail.com>
2025-03-05 20:19:25 -0300

    chore: change some bugs from variable

--- app/app.py
+++ app/app.py
@@ -1,17 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:4905@localhost:5432/gol_db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4905@localhost:5432/gol_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 from views import *
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
