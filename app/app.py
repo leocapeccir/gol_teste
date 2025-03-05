8c8ae70f7e7ad37a7c8b2a999b936b3d13a3e6ee
leocapeccir <leocapeccir@gmail.com>
2025-03-05 10:42:59 -0300

    chore: add some changes to app

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:4905@localhost:5432/gol_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:4905@localhost:5432/gol_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
